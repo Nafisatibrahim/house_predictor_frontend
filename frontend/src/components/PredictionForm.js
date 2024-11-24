48084d875ea5c405094eb197190b5f1d53be4c9f
Nafisat Ibrahim <nafisat.l@outlook.com>
2024-11-24 17:19:07 -0600

    Update PredictionForm.js

--- frontend/src/components/PredictionForm.js
+++ frontend/src/components/PredictionForm.js
@@ -53,7 +53,7 @@ function PredictionForm() {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://house-predictor-ux0x.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
