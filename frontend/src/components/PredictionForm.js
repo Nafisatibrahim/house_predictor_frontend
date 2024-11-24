a7c1179a427c93ddc15183bf456d29c5fc396c01
Nafisat Ibrahim <nafisat.l@outlook.com>
2024-11-24 17:23:43 -0600

    Update PredictionForm.js

--- frontend/src/components/PredictionForm.js
+++ frontend/src/components/PredictionForm.js
@@ -53,7 +53,7 @@ function PredictionForm() {
     }
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/predict`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
